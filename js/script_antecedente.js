3ada565ea60f4b253e05c827451635cef165b06c
María José Bravo Macías <majitobravomacias@gmail.com>
2016-09-12 19:24:10 -0500

    Está incompleto D: así que esto no vale haha
    solo están creados los archivos. luego los actualizo :smile:

---
+++ js/script_antecedente.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+    
+    $('#contenedor > div').hide();
+    $('#presentacion').show();
+    cargarListas();
+    
+    $('#insert').click(function(){
+        $('#insertar').show();
+        $('#leer').hide();
+        $('#modificar').hide();
+        $('#eliminar').hide();
+        $('#presentacion').hide();
+    });
+     $('#read').click(function(){
+        refrescar();
+        $('#leer').show();
+        $('#insertar').hide();
+        $('#modificar').hide();
+        $('#eliminar').hide();
+        $('#presentacion').hide();
+    });
+    
+    $('#update').click(function(){
+        refrescar();
+        $('#modificar').show();
+        $('#leer').hide();
+        $('#insertar').hide();
+        $('#eliminar').hide();
+        $('#presentacion').hide();
+        
+    });
+    
+    $('#delete').click(function(){
+        refrescar();
+        $('#eliminar').show();
+        $('#insertar').hide();
+        $('#leer').hide();
+        $('#modificar').hide();
+        $('#presentacion').hide();
+    });
+    
+    $("#enviarInsert").click(function(){
+        $("#insertar #resultado").empty();
+        $.post("../controlador/antecedente/controlador_antecedente_insertar.php", {provincia: $('#insertar #nombre_provincia_ciudad').val(), nombre: $('#insertar #nombre_antecedente').val()}, function(resp){
+            $("#insertar #resultado").html(resp);
+        });
+        cargarListas();
+        refrescar();
+    });
+    
+    $("#enviarUpdate").click(function(){
+        $("#modificar #resultado3").empty();
+        $.post("../controlador/antecedente/controlador_antecedente_modificar.php",{id: $('#modificar #idModificar').val(), provincia: $('#modificar #nombre_provincia_ciudad_modificar').val(), nombre: $('#modificar #nombre_antecedente').val(), estado: $('#estadoModificar option:selected').text()}, function(resp){
+            $("#modificar #resultado3").html(resp);
+    	});
+        refrescar();
+        cargarListas();
+        
+    });
+    
+    $("#enviarNombre").click(function(){
+        $("#eliminar #resultado1").empty();
+        $.post("../controlador/antecedente/controlador_antecedente_eliminar_nombre.php",{provincia: $('#eliminar #eliminaNombreProvincia').val(), nombre: $('#eliminar #eliminaNombreAntecedente').val()}, function(resp){
+        $("#eliminar #resultado1").html(resp);
+    	});
+        refrescar();
+        cargarListas();
+        $("#eliminar #resultado2").empty();
+    });
+    
+    $("#enviarId").click(function(){
+        $("#eliminar #resultado2").empty();
+        $.post("../controlador/antecedente/controlador_antecedente_eliminar_id.php",{id: $('#eliminar #eliminaIdCiudad').val()}, function(resp){
+        $("#eliminar #resultado2").html(resp);
+    	});
+          refrescar();
+          cargarListas();
+        $("#eliminar #resultado1").empty();
+    });
+    
+    /*$('#enviarInsert').click(function(){
+   $('#insertar #resultado').append("<div><?php include( 'controlador/controlador_opcion.php' ); ?></div>");
+});*/  
+});
+
+
+function refrescar(){
+    $.post("../controlador/antecedente/controlador_antecedente_consultar.php", function(resp){
+   //console.log(resp);
+        var parse = JSON.parse(resp);   
+        for(var x=0 ; x < (parse.listaopciones.length-1); x++){
+            if (x==0){
+                $(".refresh").html('');
+            }
+            $(".refresh").append('<div class="col-md-10">'+ parse.listaopciones[x].id + " " + parse.listaopciones[x].nombre + " " + parse.listaopciones[x].tipo + " " + parse.listaopciones[x].subtipo + " " + parse.listaopciones[x].estado + '</div>');
+        };
+    });
+};
+
+function cargarListas(){
+    $('#nombre_provincia_ciudad')
+    .empty();
+    $('#nombre_provincia_ciudad_modificar')
+    .empty();
+    $('#eliminaNombreProvincia')
+    .empty();
+    $('#eliminaNombreCiudad')
+    .empty();
+    $('#eliminaIdCiudad')
+    .empty();
+    $('#idModificar')
+    .empty();
+      //Llama al servicio de los usuarios y llena un select con ellas
+    $.post("../controlador/antecedente/controlador_antecedente_consultar.php", function(resp){
+        var parse = JSON.parse(resp);
+        for(var x=0 ; x < (parse.listaopciones.length-1); x++){
+            $("#eliminaNombreAntecedente").append( '<option value="'+parse.listaopciones[x].nombre+ '">'+parse.listaopciones[x].nombre+' </option>');
+            //console.log(parse.listaopciones[x].provincia);
+            $("#eliminaIdAntecedente").append( '<option value="'+parse.listaopciones[x].id+ '">'+parse.listaopciones[x].id+' </option>');
+            $("#idModificar").append( '<option value="'+parse.listaopciones[x].id+ '">'+parse.listaopciones[x].id+' </option>');
+        };
+        
+    });
+    $.post("../controlador/tipo_antecedente/controlador_tipo_antecedente_consultar.php", function(resp2){
+        var parse2 = JSON.parse(resp2);
+        for(var y=0; y < (parse2.listaopciones.length-1); y++){
+            $("#eliminaNombreProvincia").append( '<option value="'+parse2.listaopciones[y].nombre+ '">'+parse2.listaopciones[y].nombre+' </option>');
+            $("#nombre_provincia_ciudad").append( '<option value="'+parse2.listaopciones[y].nombre + '">'+parse2.listaopciones[y].nombre+' </option>');
+            //console.log(parse2.listaopciones[y].nombre);
+            $("#nombre_provincia_ciudad_modificar").append( '<option value="'+parse2.listaopciones[y].id + '">'+parse2.listaopciones[y].id+' </option>');
+            //console.log(parse2.listaopciones[y].nombre);
+        };
+        
+    });
+
+    $.post("../controlador/provincia/controlador_provincia_consultar.php", function(resp3){
+        var parse3 = JSON.parse(resp3);
+        for(var z=0; z < (parse3.listaopciones.length-1); z++){
+            $("#eliminaNombreProvincia").append( '<option value="'+parse3.listaopciones[z].nombre+ '">'+parse3.listaopciones[z].nombre+' </option>');
+            $("#nombre_provincia_ciudad").append( '<option value="'+parse3.listaopciones[z].nombre + '">'+parse3.listaopciones[z].nombre+' </option>');
+            //console.log(parse2.listaopciones[y].nombre);
+            $("#nombre_provincia_ciudad_modificar").append( '<option value="'+parse3.listaopciones[z].id + '">'+parse3.listaopciones[z].id+' </option>');
+            //console.log(parse2.listaopciones[y].nombre);
+        };
+        
+    });
+};
+
+
--- perfilAdmin.php
+++ perfilAdmin.php
@@ -158,7 +158,7 @@
                 </div>    
                 <div class="col-md-2 col-sm-6 col-xs-6">
                     <p>Antecedente</p>
-                    <a href="#" class="icono"><i class="fa fa-tags fa-4x"></i></a>
+                    <a href="vista/vista_antecedente.php" class="icono"><i class="fa fa-tags fa-4x"></i></a>
                 </div>       
                 <div class="col-md-2 col-sm-6 col-xs-6">
                     <p>Etiqueta Antecedente</p>
